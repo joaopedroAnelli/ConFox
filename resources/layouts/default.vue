d1b95f1fd62c3a6a03aa54b50f56da95b64ae409
João Anelli <joaopedroanelli99@gmail.com>
2019-09-21 11:29:06 -0300

    First Commit

---
+++ resources/layouts/default.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  b-navbar
+    template(slot="brand")
+      b-navbar-item
+        h1 ConFox
+
+
+    template(slot="start")
+      b-navbar-item Feed
+      b-navbar-item Grupos
+      b-navbar-item Chats
+      b-navbar-item Agendamentos
+
+    template(slot="end")
+      b-navbar-item(tag="item")
+        .buttons
+          a.button.is-light Sair
+
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
---
+++ resources/layouts/login.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  .hero.is-primary.is-fullheight
+    .hero-body
+      .container
+        .card
+          .header
+            h1 Login
+          .body
+            b-field(label="E-mail")
+              b-input
+
+
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+
+<style lang="stylus" scoped>
+
+  .card
+    padding 2em
+    border none
+    border-radius 5px
+    width 500px
+    max-width 80vw
+
+    .header
+      border-bottom 1px solid gray
+      margin-bottom 1em
+      h1
+        font-size 1.5em
+
+</style>
